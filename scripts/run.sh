a78f465446c02ccf80281f41cf5a6c45071caf0f
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-08-17 14:26:54 -0400

    Added extra commands for multi-assembly genomes

--- scripts/run.sh
+++ scripts/run.sh
@@ -74,6 +74,7 @@ gzcat ~/data/genomes/${FROM_GENOME}.fa.gz \
    | gzip > data/${FILE_ID}.fastq.gz
 echo "$(date) Intermediate... generated reads ${FILE_ID}.contacts.genome" >> status
 
+
 bwa aln -t 4 ~/data/genomes/${TO_GENOME}.fa.gz \
     data/${FILE_ID}.fastq.gz \
     | bwa samse -n ${NUM_DUPS} ~/data/genomes/${FROM_GENOME}.fa.gz - \
