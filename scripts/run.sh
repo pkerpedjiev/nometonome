706fd7e32c6c98bc13ad984ac76d7d4318c01b76
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2019-11-27 22:22:59 -0800

    Use the run command

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,70 +1,52 @@
-COMMAND="./run.sh FROM_GENOME TO_GENOME NUM_READS READ_LENGTH NUM_DUPS BIN_SIZE"
+set -e
+set -x
+
+COMMAND="./run.sh GENOME_PATH NUM_READS READ_LENGTH NUM_DUPS BIN_SIZE SEQUENCING_ERROR_RATE"
 
 trap "exit" INT
 
 if [[ -z "$1" ]]; then
-    echo "FROM_GENOME not provided"
+    echo "GENOME_PATH"
     echo "$COMMAND"
     exit
 fi
 
 if [[ -z "$2" ]]; then
-    echo "TO_GENOME not provided"
-    echo "$COMMAND"
-    exit
-fi
-
-if [[ -z "$3" ]]; then
     echo "NUM_READS not provided"
     echo "$COMMAND"
     exit
 fi
 
-if [[ -z "$4" ]]; then
+if [[ -z "$3" ]]; then
     echo "READ_LENGTH not provided"
     echo "$COMMAND"
     exit
 fi
 
-if [[ -z "$5" ]]; then
+if [[ -z "$4" ]]; then
     echo "NUM_DUPS not provided"
     echo "$COMMAND"
     exit
 fi
 
-if [[ -z "$6" ]]; then
+if [[ -z "$5" ]]; then
     echo "BIN_SIZE not provided"
     echo "$COMMAND"
     exit
 fi
 
-if [[ -z "$7" ]]; then
+if [[ -z "$6" ]]; then
     echo "SEQUENCING_ERROR_RATE not provided"
     echo "$COMMAND"
     exit
 fi
 
-if [[ -z "$8" ]]; then
-    echo "CHROMSIZES_FILE not provided"
-    echo "$COMMAND"
-    exit
-fi
-
-if [[ ! -f "$8" ]]; then
-    echo "CHROMSIZES_FILE doesn't exist: $8"
-    echo "$COMMAND"
-    exit
-fi
-
-
-FROM_GENOME=$1
-TO_GENOME=$2
-NUM_READS=$3
-READ_LENGTH=$4
-NUM_DUPS=$5
-BIN_SIZE=$6
-SEQUENCING_ERROR_RATE=$7
-CHROMSIZES_FILE=$8
+GENOME_PATH=$1
+NUM_READS=$2
+READ_LENGTH=$3
+NUM_DUPS=$4
+BIN_SIZE=$5
+SEQUENCING_ERROR_RATE=$6
 
 echo "SEQUENCING_ERROR_RATE:", $SEQUENCING_ERROR_RATE
 echo $4
@@ -73,8 +55,14 @@ echo $6
 echo $7
 echo $8
 
-echo "FROM_GENOME:" $FROM_GENOME
-echo "TO_GENOME" $TO_GENOME
+filename=$(basename -- "$GENOME_PATH")
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+GENOME_NAME=$filename
+
+echo "GENOME_PATH:" $GENOME_PATH
+echo "GENOME_NAME:" $GENOME_NAME
 echo "NUM_READS" $NUM_READS
 echo "READ_LENGTH" $READ_LENGTH
 echo "NUM_DUPS" $NUM_DUPS
@@ -82,14 +70,23 @@ echo "BIN_SIZE" $BIN_SIZE
 echo "SEQUENCING_ERROR_RATE" $SEQUENCING_ERROR_RATE
 
 
-FILE_ID=${FROM_GENOME}.${TO_GENOME}.${BIN_SIZE}.${READ_LENGTH}.${NUM_READS}.${NUM_DUPS}.${SEQUENCING_ERROR_RATE}
+FILE_ID=${GENOME_NAME}.${BIN_SIZE}.${READ_LENGTH}.${NUM_READS}.${NUM_DUPS}.${SEQUENCING_ERROR_RATE}
 
 echo $FILE_ID
 
 echo "$(date) Started ${FILE_ID}.contacts.genome" >> status
 
+if [ ! -f "${GENOME_PATH}.ann" ]; then
+    bwa index ${GENOME_PATH}
+fi
+
+CHROMSIZES_FILE=/tmp/chrom.sizes
+
+create_chrominfo.py ${GENOME_PATH} > ${CHROMSIZES_FILE}.raw
+awk '{ print $1 "\t" $NF }' ${CHROMSIZES_FILE}.raw > ${CHROMSIZES_FILE}
+
 if true; then
-    gzcat ~/data/genomes/${FROM_GENOME}.fa.gz \
+    gzcat ${GENOME_PATH} \
        | python scripts/generate_reads.py \
        --read-length ${READ_LENGTH} \
        --num-reads ${NUM_READS}  - \
@@ -98,15 +95,14 @@ if true; then
     echo "$(date) Intermediate... generated reads ${FILE_ID}.contacts.genome" >> status
 
 
-    bwa aln -t 4 ~/data/genomes/${TO_GENOME}.fa.gz \
+    bwa aln -t 4 ${GENOME_PATH} \
         data/${FILE_ID}.fastq.gz \
-        | bwa samse -n ${NUM_DUPS} ~/data/genomes/${TO_GENOME}.fa.gz - \
+        | bwa samse -n ${NUM_DUPS} ${GENOME_PATH} - \
         data/${FILE_ID}.fastq.gz \
         | gzip > aligned/${FILE_ID}.sam.gz
 
     gzcat aligned/${FILE_ID}.sam.gz \
         | python scripts/parse_sam.py - \
-        ${FROM_GENOME} ${TO_GENOME} \
         | gzip > contacts/${FILE_ID}.contacts.gz
 
     echo "$(date) Intermediate... parsed SAM ${FILE_ID}.contacts.genome" >> status
@@ -115,19 +111,17 @@ if true; then
 
     cooler makebins \
         ${CHROMSIZES_FILE} \
-      ${BIN_SIZE} -o bins/${FROM_GENOME}.${BIN_SIZE}.bins
-
-    cooler csort --nproc 4 -c1 1 -p1 2 -s1 3 \
-      -c2 4 -p2 5 -s2 6 \
-      contacts/${FILE_ID}.contacts.gz \
-      ${CHROMSIZES_FILE}              \
-      -o contacts/${FILE_ID}.contacts.sorted
+      ${BIN_SIZE} -o bins/${GENOME_NAME}.${BIN_SIZE}.bins
 fi
 
+echo "Loading pairs"
+
 cooler cload pairs \
-  -c1 1 -p1 2 -c2 4 -p2 5 \
-  bins/${FROM_GENOME}.${BIN_SIZE}.bins \
+  -c1 1 -p1 2 -c2 3 -p2 4 \
+  bins/${GENOME_NAME}.${BIN_SIZE}.bins \
   contacts/${FILE_ID}.contacts.gz \
   coolers/${FILE_ID}.cool
 
-cooler zoomify coolers/${FILE_ID}.cool --no-balance
+echo "Zoomifying"
+
+cooler zoomify coolers/${FILE_ID}.cool
